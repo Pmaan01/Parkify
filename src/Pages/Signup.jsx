fe3a103de7d642eec63b1bab2d5c07486cb2b90e
Nishkarmanjit Kaur <000482093@student.vcc.ca>
2025-06-06 13:46:08 -0700

    Update API URLs to deployed backend

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -16,7 +16,7 @@ export default function Signup() {
         }
 
         try {
-            const res = await axios.post("http://localhost:5000/api/auth/signup", {
+            const res = await axios.post("https://parkify-backend-t4ju.onrender.com/api/auth/signup", {
                 name,
                 email,
                 password,
